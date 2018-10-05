1acba2772169da9c84a2302d4101ac0b95e13f8d
haigbedros <haigbedros@gmail.com>
2018-10-05 17:47:43 -0400

    burger button js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+$(document).ready(function(){
+  var count=0;
+  $(".toggle").click(function(){
+    if(count % 2 == 0){
+      $(".toggle_div").css("height","250px");
+      $(".toggle_div ul li").css({"display":"block", "float": "initial", "position": "relative", "left": "48px", "top": "40px"});
+      $(".toggle_div ul li:last-child").css({"left":"16px","top":"14px"});
+      count++;
     }
-}
+    else {
+      $(".toggle_div").css("height","80px");
+      $(".toggle_div ul li").css({"display":"none"});
+      count++;
+    }
+    });
+})
